3cde9e876798866839ed2d65ddfddae39a2d9b43
Nikhil <nikhil462001@gmail.com>
2025-03-07 13:53:12 +0530

    some project jsx changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import projects from '../projectData/project'
 
 const Project = () => {
   return (
@@ -12,46 +13,21 @@ const Project = () => {
         </div>
 
         <div className='d-flex justify-content-around gap-5 gap-lg-0 p-4 flex-wrap'>
-
-          <div className="card">
-            <img src="/portfolio/e-learning.jpg" className="card-img-top" height="200px" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title fw-bold text-center">E-Course Site</h5>
-              <h6 className='text-muted'>React, Node.js, Express, MySql</h6>
-              <p className="card-text mt-2 mb-2" style={{ textAlign: "justify" }}>This web page is a part of my portfolio. It is a simple React application that displays a simple web page with a navigation bar and a few sections.</p>
-              <div className='d-flex justify-content-center gap-3'>
-                <a href="https://github.com/Ng462001/ngtech" target='_blank' className="btn btn-secondary"> <i className="bi bi-github"></i> GitHub</a>
-                <a href="https://ngtech-olive.vercel.app/" target='_blank' className="btn btn-success"><i className="bi bi-link"></i> Link</a>
+          {projects.map((project, index) => (
+            <div className="card mt-3" key={index}>
+              <img src={project.image_src} className="card-img-top" height="200px" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title fw-bold text-center">{project.title}</h5>
+                <h6 className='text-muted'>{project.language}</h6>
+                <p className="card-text mt-2 mb-2" style={{ textAlign: "justify" }}>{project.desc}</p>
+                <div className='d-flex justify-content-center gap-3 mt-3'>
+                  <a href={project.github_link} target='_blank' className="btn btn-outline-secondary"> <i className="bi bi-github"></i> GitHub</a>
+                  <a href={project.website_link} target='_blank' className="btn btn-outline-success"><i className="bi bi-link"></i> Link</a>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="card">
-            <img src="/portfolio/job-search.jpg" className="card-img-top" height="200px" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title fw-bold text-center">Job Search Website</h5>
-              <h6 className='text-muted'>HTML, CSS, Bootstrap, React</h6>
-              <p className="card-text mt-2 mb-2" style={{ textAlign: "justify" }}>This web page is a part of my portfolio. It is a simple React application that displays a simple web page with a navigation bar and a few sections.</p>
-              <div className='d-flex justify-content-center gap-3'>
-                <a href="https://github.com/Ng462001/jobs-project" target='_blank' className="btn btn-secondary"> <i className="bi bi-github"></i> GitHub</a>
-                <a href="https://jobsearchproject.vercel.app/" target='_blank' className="btn btn-success"><i className="bi bi-link"></i> Link</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card">
-            <img src="/portfolio/e-learning-html.png" className="card-img-top" height="200px" alt="..." />
-            <div className="card-body w-100">
-              <h5 className="card-title fw-bold text-center">E-learning Website</h5>
-              <h6 className='text-muted'>HTML, CSS, JavaScript, Bootstrap</h6>
-              <p className="card-text mt-2 mb-2" style={{ textAlign: "justify" }}>This web page is a part of my portfolio. It is a simple React application that displays a simple web page with a navigation bar and a few sections.</p>
-              <div className='d-flex justify-content-center gap-3'>
-                <a href="https://github.com/Ng462001/e-learning-site" target='_blank' className="btn btn-secondary"> <i className="bi bi-github"></i> GitHub</a>
-                <a href="https://e-learning-project-live.vercel.app/" target='_blank' className="btn btn-success"><i className="bi bi-link"></i> Link</a>
-              </div>
-            </div>
-          </div>
-
+          ))
+          }
         </div>
       </section>
     </>
---
+++ src/projectData/project.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        title: "E-Course Site",
+        language: "React, Node.js, Express, MySql",
+        desc: "This web page is a part of my portfolio. It is a simple React application that displays a simple web page with a navigation bar and a few sections.",
+        image_src: "/portfolio/e-learning.jpg",
+        github_link: "https://github.com/Ng462001/ngtech",
+        website_link: "https://ngtech-olive.vercel.app/"
+    },
+    {
+        title: "Job Search Website",
+        language: "HTML, CSS, Bootstrap, React",
+        image_src: "/portfolio/job-search.jpg",
+        desc: "This web page is a part of my portfolio. It is a simple React application that displays a simple web page with a navigation bar and a few sections.",
+        github_link: "https://github.com/Ng462001/jobs-project",
+        website_link: "https://jobsearchproject.vercel.app/"
+    },
+    {
+        title: "E-learning Website",
+        language: "HTML, CSS, JavaScript, Bootstrap",
+        image_src: "/portfolio/e-learning-html.png",
+        desc: "This web page is a part of my portfolio. It is a simple React application that displays a simple web page with a navigation bar and a few sections.",
+        github_link: "https://github.com/Ng462001/e-learning-site",
+        website_link: "https://e-learning-project-live.vercel.app/"
+    }
+]
+
+export default projects
